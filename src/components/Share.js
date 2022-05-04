8f2a3b09dc51ef8475a50cc77487b60d7ff81fd0
Christy La Guardia <christy.laguardia@classpass.com>
2022-05-04 00:53:16 -0600

    fix share component

--- src/components/Share.js
+++ src/components/Share.js
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types';
 import { useRouter } from 'next/router';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
+// TODO: sometimes adblockers won't show this component
+
 export default function Share(props) {
   const { title } = props;
+
   const {
     query: { slug },
   } = useRouter();
+
   const shareUrl = `https://christylaguardia.com/blog/${slug}`;
+  const twitterUrl = `https://twitter.com/intent/tweet?url=${shareUrl}&text=${title}`;
+  const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${shareUrl}`;
+  const linkedInUrl = `https://www.linkedin.com/shareArticle?mini=true&url=${shareUrl}&title=${title}`;
 
   return (
     <div className="share-container">
@@ -25,28 +32,15 @@ export default function Share(props) {
       </p>
       <p>
         <span>Share this article on </span>
-        <a
-          className="twitter-share-button"
-          target="_blank"
-          rel="noreferrer"
-          href={`https://twitter.com/intent/tweet?url=${shareUrl}&text=${title}`}
-        >
+        <a target="_blank" rel="noreferrer" href={twitterUrl}>
           Twitter
         </a>
         <span> &#8226; </span>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href={`https://www.facebook.com/sharer/sharer.php?u=${shareUrl}`}
-        >
+        <a target="_blank" rel="noreferrer" href={facebookUrl}>
           Facebook
         </a>
         <span> &#8226; </span>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href={`https://www.linkedin.com/shareArticle?mini=true&url=${shareUrl}&title=${title}`}
-        >
+        <a target="_blank" rel="noreferrer" href={linkedInUrl}>
           LinkedIn
         </a>
         <span> &#8226; </span>
